089322f6e7922caf4a7ca5e45442b3ea2a3f5282
jlccn <jlccn@users.noreply.github.com>
2021-01-31 11:27:38 +0800

    Update jd_joy_feed.py

--- jd_joy_feed.py
+++ jd_joy_feed.py
@@ -2,7 +2,7 @@ import jdCookie
 import json
 import requests
 import time
-
+from datetime import datetime,timedelta
 
 FEED_NUM = 10   # [10,20,40,80]
 
@@ -23,10 +23,17 @@ def getTemplate(cookies, functionId, params):
 def feed(cookies, feedCount):
     data = getTemplate(cookies, "feed", (('feedCount', str(feedCount)),))
     print("\n【feed】\n ", data["errorCode"])
+    
+def printTime():
+	t = datetime.utcnow() 
+	t2 = t + timedelta(hours=8)
+	print('utc时间：', t.strftime("%Y-%m-%d %H:%M:%S"))
+	print('beijing时间：',t2.strftime("%Y-%m-%d %H:%M:%S"))
 
 def run():
-    for cookies in jdCookie.get_cookies():
-        feed(cookies, FEED_NUM) 
+    printTime()
+    #for cookies in jdCookie.get_cookies():
+    #    feed(cookies, FEED_NUM) 
        
 if __name__ == "__main__":
     run()
